2917c905e0c14fa93ed05bb40d407441e176211c
Julio Martins <juliocrmartins@gmail.com>
2020-04-28 14:31:12 -0300

    Tela de login

--- views-cache/header.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
+++ views-cache/header.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
@@ -209,7 +209,7 @@ desired effect
                   <a href="#" class="btn btn-default btn-flat">Profile</a>
                 </div>
                 <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                  <a href="/admin/logout" class="btn btn-default btn-flat">Sign out</a>
                 </div>
               </li>
             </ul>
@@ -274,4 +274,3 @@ desired effect
     </section>
     <!-- /.sidebar -->
   </aside>
-  
