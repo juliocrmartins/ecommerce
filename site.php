061dd784bfd3b23ae018f049947b7dce0c493819
Julio Martins <juliocrmartins@gmail.com>
2020-05-02 17:07:08 -0300

    Carrinho de Compras - Calculo de Frete

--- site.php
+++ site.php
@@ -68,7 +68,8 @@ $app->get("/cart", function(){
 
 	$page->setTpl("cart", [
 		"cart"=>$cart->getValues(),
-		"products"=>$cart->getProducts()
+		"products"=>$cart->getProducts(),
+		"error"=>$cart->getMsgError()
 	]);
 });
 
@@ -118,6 +119,16 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	header("Location: /cart");
 	exit;
 });
+
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST["zipcode"]);
+
+	header("Location: /cart");
+	exit;
+})
 
  ?>
 
