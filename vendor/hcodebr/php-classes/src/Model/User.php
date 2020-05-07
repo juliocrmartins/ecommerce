a498daabb1ab489d43ff28aa24af6f87798302c6
Julio Martins <juliocrmartins@gmail.com>
2020-05-06 23:04:30 -0300

    Meus Pedidos e Resumo Carrinho

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -370,6 +370,24 @@ class User extends Model
 		return password_hash($password, PASSWORD_DEFAULT, [
 			"cost"=>12
 		]);
+	}
+
+	public function getOrders()
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT * FROM tb_orders a 
+			INNER JOIN tb_ordersstatus b USING(idstatus)
+			INNER JOIN tb_carts c USING(idcart)
+			INNER JOIN tb_users d ON d.iduser = a.iduser
+			INNER JOIN tb_addresses e USING(idaddress)
+			INNER JOIN tb_persons f ON f.idperson = d.idperson
+			WHERE a.iduser = :iduser",[
+				":iduser"=>$this->getiduser()
+			]);
+
+		return $results;
 	}	
 }
 
