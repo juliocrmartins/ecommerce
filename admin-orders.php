b6a8bb07df6bdc94c21ef447168f013def7ba664
Julio Martins <juliocrmartins@gmail.com>
2020-05-09 13:05:04 -0300

    Admin, Pedidos, Filtro e paginação

--- admin-orders.php
+++ admin-orders.php
@@ -85,10 +85,30 @@ $app->get("/admin/orders", function(){
 
 	User::verifyLogin();
 
+	$search = isset($_GET["search"]) ? $_GET["search"] : "";
+	$page = isset($_GET["page"]) ? (int)$_GET["page"] : 1;
+
+	$pagination = Order::getOrdersPage($search, $page);
+
+	$pages = [];
+
+	for ($x = 1; $x <= $pagination["pages"]; $x++) { 
+		
+		array_push($pages, [
+			"href"=>"/admin/orders?" . http_build_query([
+				"page"=>$x,
+				"search"=>$search
+			]),
+			"text"=>$x
+		]);
+	}
+
 	$page = new PageAdmin();
 
 	$page->setTpl("orders", [
-		"orders"=>Order::listAll()
+		"orders"=>$pagination["data"],
+		"search"=>$search,
+		"pages"=>$pages
 	]);
 });
 
